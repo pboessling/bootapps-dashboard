5e9ebf35d507b65eea52594a4a2963a0ff2f6271
Philippe Boessling <phib@phib.de>
2018-11-11 09:59:11 +0100

    Fixed unit test class.

--- src/test/java/BootAppsDashboardApplicationTests.java
+++ src/test/java/BootAppsDashboardApplicationTests.java
@@ -1,10 +1,11 @@
+import de.phib.bootapps.dashboard.BootappsDashboardApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = BootappsDashboardApplication.class)
 public class BootAppsDashboardApplicationTests {
 
 	@Test
